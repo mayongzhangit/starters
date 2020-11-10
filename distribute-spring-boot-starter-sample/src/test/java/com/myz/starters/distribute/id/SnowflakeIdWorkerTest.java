a6d3f5991b029450fb24a1ecd3369f1ee0d5039a
yzMa <2641007740@qq.com>
2020-11-10 21:52:32 +0800

    sharddb count snowflake no concurrent  odd

--- distribute-spring-boot-starter-sample/src/test/java/com/myz/starters/distribute/id/SnowflakeIdWorkerTest.java
+++ distribute-spring-boot-starter-sample/src/test/java/com/myz/starters/distribute/id/SnowflakeIdWorkerTest.java
@@ -24,19 +24,47 @@ public class SnowflakeIdWorkerTest {
     @Autowired
     private SnowflakeIdWorker snowflakeIdWorker;
 
+    /**
+     * 我擦，就这数据来看，并发小的时候以为SnowFlake的最后几位是从0 开始 每次并发+1，没有并发也就是0，导致产生的id是偶数
+     * 当使用分库分表用了0数个db的时候就很蛋疼，特别是用了2个db的时候，没有什么并发的情况下，数据全部达到了db0上，如果db0的表的数量也是2个的话，全达到了table0上  。。。。
+     * @throws InterruptedException
+     */
     @Test
-    public void nextId(){
-        long id = snowflakeIdWorker.nextId();
-        id = snowflakeIdWorker.nextId();
-        id = snowflakeIdWorker.nextId();
-        id = snowflakeIdWorker.nextId();
-        id = snowflakeIdWorker.nextId();
-        id = snowflakeIdWorker.nextId();
-        id = snowflakeIdWorker.nextId();
-        id = snowflakeIdWorker.nextId();
+    public void nextId() throws InterruptedException {
 
+        log.info("id={}",snowflakeIdWorker.nextId());
+        log.info("id={}",snowflakeIdWorker.nextId());
+
+        Thread.sleep(10);
+        log.info("after sleep id={}",snowflakeIdWorker.nextId());
+
+        log.info("id={}",snowflakeIdWorker.nextId());
+        log.info("id={}",snowflakeIdWorker.nextId());
+        log.info("id={}",snowflakeIdWorker.nextId());
+        log.info("id={}",snowflakeIdWorker.nextId());
+        log.info("id={}",snowflakeIdWorker.nextId());
+
+
+        Thread.sleep(10);
+        log.info("after sleep id={}",snowflakeIdWorker.nextId());
+        log.info("id={}",snowflakeIdWorker.nextId());
+        log.info("id={}",snowflakeIdWorker.nextId());
+
+        Thread.sleep(10);
+        log.info("after sleep id={}",snowflakeIdWorker.nextId());
+        log.info("id={}",snowflakeIdWorker.nextId());
+        log.info("id={}",snowflakeIdWorker.nextId());
+
+
+        Thread.sleep(10);
+        log.info("after sleep id={}",snowflakeIdWorker.nextId());
+        log.info("id={}",snowflakeIdWorker.nextId());
+        log.info("id={}",snowflakeIdWorker.nextId());
+
+        long id = snowflakeIdWorker.nextId();
         SnowflakeInfo snowflakeInfo = snowflakeIdWorker.extraId(id);
         // 这咋Assert
+
         log.info("id={}",id);
         log.info("snowflakeInfo={}",snowflakeInfo);
     }
