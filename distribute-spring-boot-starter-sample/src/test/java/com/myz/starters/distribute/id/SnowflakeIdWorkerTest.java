3b48fbc90794b5959e1a009e13ff77bc3541bdd1
yzMa <2641007740@qq.com>
2020-11-18 11:12:18 +0800

    slot ex use cluster.nodes done well

--- distribute-spring-boot-starter-sample/src/test/java/com/myz/starters/distribute/id/SnowflakeIdWorkerTest.java
+++ distribute-spring-boot-starter-sample/src/test/java/com/myz/starters/distribute/id/SnowflakeIdWorkerTest.java
@@ -24,6 +24,18 @@ public class SnowflakeIdWorkerTest {
     @Autowired
     private SnowflakeIdWorker snowflakeIdWorker;
 
+    @Test
+    public void shard() throws InterruptedException {
+
+        for (int i =0;i<1100;i++){
+            Thread.sleep(2000);
+            long id = snowflakeIdWorker.nextId();
+            SnowflakeInfo snowflakeInfo = snowflakeIdWorker.extraId(id);
+            long actTable = id  % 9;
+            log.info("actTable={} idInfo={}",actTable,snowflakeInfo);
+        }
+    }
+
     /**
      * 我擦，就这数据来看，并发小的时候以为SnowFlake的最后几位是从0 开始 每次并发+1，没有并发也就是0，导致产生的id是偶数
      * 当使用分库分表用了0数个db的时候就很蛋疼，特别是用了2个db的时候，没有什么并发的情况下，数据全部达到了db0上，如果db0的表的数量也是2个的话，全达到了table0上  。。。。
---
+++ distribute-spring-boot-starter-sample/src/test/java/com/myz/starters/distribute/lock/RedisLockTemplateTest.java
@@ -0,0 +1,10 @@
+package com.myz.starters.distribute.lock;
+
+/**
+ * @author yzMa
+ * @desc
+ * @date 2020/11/18 10:25
+ * @email 2641007740@qq.com
+ */
+public class RedisLockTemplateTest {
+}
