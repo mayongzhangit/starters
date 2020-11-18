f83693c0859cfe8ddd0c036075b32f6c26030689
yzMa <2641007740@qq.com>
2020-11-18 20:31:28 +0800

    x

--- distribute-spring-boot-starter/src/main/java/com/myz/starters/distribute/DistributeLockAutoConfiguration.java
+++ distribute-spring-boot-starter/src/main/java/com/myz/starters/distribute/DistributeLockAutoConfiguration.java
@@ -1,10 +1,16 @@
 package com.myz.starters.distribute;
 
-import com.myz.starters.distribute.lock.RedisLockConfiguration;
+import com.myz.starters.distribute.lock.*;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import redis.clients.jedis.JedisCluster;
 
 /**
  * @author yzMa
@@ -14,6 +20,25 @@ import org.springframework.context.annotation.Import;
  */
 @Configuration
 @AutoConfigureAfter(RedisAutoConfiguration.class)
-@Import(RedisLockConfiguration.class)
+//@Import(RedisLockConfiguration.class)// 这样会直接import到beanFactory中，比xxAutoConfiguration这样的永远早
 public class DistributeLockAutoConfiguration {
+
+
+    @Primary
+    @ConditionalOnBean(JedisCluster.class)
+    public JedisClusterLock jedisClusterLock(JedisCluster jedisCluster){
+        return new JedisClusterLock(jedisCluster);
+    }
+
+    @Bean
+    @ConditionalOnBean(StringRedisTemplate.class)
+    public RedisTemplateLock redisTemplateLock(StringRedisTemplate stringRedisTemplate){
+        return new RedisTemplateLock(stringRedisTemplate);
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "distribute.lock.redis",name = "enabled")
+    public RedisLockTemplate redisLockTemplate(RedisLock redisLock){
+        return new RedisLockTemplate(redisLock);
+    }
 }
--- distribute-spring-boot-starter/src/main/java/com/myz/starters/distribute/lock/RedisLockConfiguration.java
+++ distribute-spring-boot-starter/src/main/java/com/myz/starters/distribute/lock/RedisLockConfiguration.java
@@ -1,37 +1,37 @@
-package com.myz.starters.distribute.lock;
-
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import redis.clients.jedis.JedisCluster;
-
-/**
- * @author yzMa
- * @desc
- * @date 2020/11/6 16:14
- * @email 2641007740@qq.com
- */
-@Configuration
-@ConditionalOnProperty(prefix = "distribute.lock",name = "enabled")
-public class RedisLockConfiguration {
-
-    @Primary
-    @ConditionalOnBean(JedisCluster.class)
-    public JedisClusterLock jedisClusterLock(JedisCluster jedisCluster){
-        return new JedisClusterLock(jedisCluster);
-    }
-
-    @Bean
-    @ConditionalOnBean(StringRedisTemplate.class)
-    public RedisTemplateLock redisTemplateLock(StringRedisTemplate stringRedisTemplate){
-        return new RedisTemplateLock(stringRedisTemplate);
-    }
-
-    @Bean
-    public RedisLockTemplate redisLockTemplate(RedisLock redisLock){
-        return new RedisLockTemplate(redisLock);
-    }
-}
+//package com.myz.starters.distribute.lock;
+//
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//import redis.clients.jedis.JedisCluster;
+//
+///**
+// * @author yzMa
+// * @desc
+// * @date 2020/11/6 16:14
+// * @email 2641007740@qq.com
+// */
+//@Configuration
+//@ConditionalOnProperty(prefix = "distribute.reids.lock",name = "enabled")
+//public class RedisLockConfiguration {
+//
+//    @Primary
+//    @ConditionalOnBean(JedisCluster.class)
+//    public JedisClusterLock jedisClusterLock(JedisCluster jedisCluster){
+//        return new JedisClusterLock(jedisCluster);
+//    }
+//
+//    @Bean
+//    @ConditionalOnBean(StringRedisTemplate.class)
+//    public RedisTemplateLock redisTemplateLock(StringRedisTemplate stringRedisTemplate){
+//        return new RedisTemplateLock(stringRedisTemplate);
+//    }
+//
+//    @Bean
+//    public RedisLockTemplate redisLockTemplate(RedisLock redisLock){
+//        return new RedisLockTemplate(redisLock);
+//    }
+//}
